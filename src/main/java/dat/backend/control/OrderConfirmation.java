d8de24afddbd215b542fde7a40ceb9debf1b5a42
RasmanHuhu <112378651+RasmanHuhu@users.noreply.github.com>
2023-03-27 11:16:56 +0200

    order confirmation servlet og jsp startet

---
+++ src/main/java/dat/backend/control/OrderConfirmation.java
@@ -0,0 +1,34 @@
+package dat.backend.control;
+
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.Order;
+import dat.backend.model.persistence.ConnectionPool;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "OrderConfirmation", value = "/OrderConfirmation")
+public class OrderConfirmation extends HttpServlet {
+
+    private ConnectionPool connectionPool;
+    /* private Order currentOrder; */
+
+    @Override
+    public void init() throws ServletException
+    {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+      /*  currentOrder = new Order(); */
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
