e1d49c5313aa6194013ef0236f653dccf4524189
Kussemar <marcus.dofinsmurt@gmail.com>
2023-03-28 13:26:12 +0200

    admin fix fix victor stille

--- src/main/java/dat/backend/model/persistence/AdminFacade.java
+++ src/main/java/dat/backend/model/persistence/AdminFacade.java
@@ -1,11 +1,20 @@
 package dat.backend.model.persistence;
 
 import dat.backend.model.entities.User;
+import dat.backend.model.exceptions.DatabaseException;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class AdminFacade {
-   public static void addMoney(ConnectionPool connectionPool, User user, int money) throws SQLException {
+   public static void addMoney(ConnectionPool connectionPool, User user, int money) throws DatabaseException {
     AdminMapper.addMoney(connectionPool,user,money);
+    }
+
+    public static List<User> getAllUsers(ConnectionPool connectionPool) throws DatabaseException {
+       return AdminMapper.getAllUsers(connectionPool);
+    }
+    public static User getUserFromUsername(ConnectionPool connectionPool, String username) throws DatabaseException{
+       return AdminMapper.getUserFromUsername(connectionPool, username);
     }
 }
